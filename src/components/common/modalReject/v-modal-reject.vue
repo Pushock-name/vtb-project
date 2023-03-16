4c392ee5bf4a150c68a34bfbfccc5f1a8c5584fa
Pushock <pzabolotnyi@gmail.com>
2023-03-16 14:37:37 +0400

    added modal window

---
+++ src/components/common/modalReject/v-modal-reject.vue
@@ -0,0 +1,51 @@
+<template lang="">
+    <div class="container">
+        <div class="v-modal-reject">
+            <h2>Подсказка</h2>
+            <p>Мы не нашли такого пункта назначения в списке. Пожалуйста, попробуйте ещё раз.</p>
+            <button class="v-modal-reject__btn">Поехали!</button>
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'v-modal-reject'
+}
+</script>
+
+<style lang="scss">
+    .v-modal-reject {
+        background: #0D0D0D;
+        border-radius: 8px;
+        background-image: url(../../assets/images/background2.png);
+        width: 50%;
+        @media (max-width: 767px) {
+            width: 100%;
+        }
+
+    }
+    .v-modal-reject h2 {
+        margin-top: 58px;
+        font-size: 22px;
+        line-height: 110%;    
+        text-shadow: 0px 0px 4px rgba(31, 107, 255, 0.35);
+    }
+
+    .v-modal-reject p {
+        margin-top: 16px;
+        font-weight: 500;
+        font-size: 18px;
+    }
+
+    .v-modal-reject__btn {
+        width: 343px;
+        height: 60px;
+        background: linear-gradient(0deg, #F4F7FA, #F4F7FA), linear-gradient(90deg, #1437F5 0%, #3983F7 100%);
+        border-radius: 8px;
+        font-weight: 500;
+        font-size: 18px;
+        line-height: 30px;
+        color: #000000;
+    }
+</style>
